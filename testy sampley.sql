7df4c8fa8c15be120bc5fdfecede8e2eae717b40
bag3318 <bglatman@outlook.com>
2016-12-02 20:24:33 -0800

    jhkklh

--- testy sampley.sql
+++ testy sampley.sql
@@ -5,9 +5,11 @@ create procedure insert_message()
 begin
 
 declare ID smallint;
+
 declare rate smallint;
 declare usrmsg varchar(888);
 
 insert into message (rating, usrmessage) values (ID, rate, usrmsg);
-
+set ID = select LAST_INSERT_ID();
+return ID;
 end;
