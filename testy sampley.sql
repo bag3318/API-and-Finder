76c228a7ca342cd83c9c4d1cf75a725c76e21036
bag3318 <bglatman@outlook.com>
2016-12-02 20:26:53 -0800

    kasdfj

--- testy sampley.sql
+++ testy sampley.sql
@@ -10,6 +10,6 @@ declare rate smallint;
 declare usrmsg varchar(888);
 
 insert into message (rating, usrmessage) values (ID, rate, usrmsg);
-set ID = select LAST_INSERT_ID();
+select LAST_INSERT_ID();
 return ID;
 end;
